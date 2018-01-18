aaa7afcf4be4f099b0a8028e24ceb14982956e2d
Jamie Williams <jwilliams492@gmail.com>
2018-01-17 23:36:24 -0800

    completed reader solution

--- lab-jamie/lib/reader.js
+++ lab-jamie/lib/reader.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const fs = require('fs');
+let i = 0;
+
+module.exports = (paths, callback) => {
+  do {
+    fs.readFile(paths[i], (err, data) => {
+      return callback(err) ? callback(null, data.toString()) : undefined;
+    });
+    i++;
+  } while (i < paths.length);
+};
